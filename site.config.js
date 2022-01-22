0afd8585259bb2de6ed391d2ce76e78b4c1a8f98
ashao0425 <alexshao20050425@163.com>
2022-01-23 02:23:56 +0800

    GalRC documentation Ver 1.0

---
+++ site.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  // where it all starts -- the site's root Notion page (required)
+  rootNotionPageId: '98226f078ed1422c83f9673377a7b6a7',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: '📄 GalRC Docs',
+  domain: 'galrcdocs.vercel.app',
+  author: 'GalRC Developers',
+
+  // open graph metadata (optional)
+  description: 'GalRC official documentation',
+  socialImageTitle: 'GalRC',
+  socialImageSubtitle: '⚛ Welcome to GalRC',
+
+  // social usernames (optional)
+  github: 'GalRC',
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // image CDN host to proxy all image requests through (optional)
+  // NOTE: this requires you to set up an external image proxy
+  imageCDNHost: null,
+
+  // Utteranc.es comments via GitHub issue comments (optional)
+  utterancesGitHubRepo: null,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  // NOTE: this requires you to set up Google Firebase and add the environment
+  // variables specified in .env.example
+  isPreviewImageSupportEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null
+}
